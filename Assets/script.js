076548aa81e53ac6c86652691e73b9ceded1a748
Daniel Son <sonjdaniel@gmail.com>
2023-03-19 20:11:49 -0500

    Trying to have user enter prompt message and see if it echo back

--- Assets/script.js
+++ Assets/script.js
@@ -13,8 +13,24 @@ function writePassword() {
 // Start of code
 function generatePassword() {
 
+// characters special character, number, upper case and lower case
+var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lower = ["a","b","c","d","e","f","g", "h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var spec = ["!","@","#","$","%","^","&","*","(",")","_","+","~","/","?","`","-",":",";","<",">"];
 
 
+//  prompt user for the length and echo back
+var length = prompt("How long do you want your password to be? pick between 8-128");
+console.log(length);
+
+
+
+// prompt user to enter information
+var char = confirm("Do you want special character? Press Ok for Yes or Cancel for No")
+var num = confirm("Do you want to include numbers? Press Ok for Yes or Cancel for No ")
+var upper = confirm("Do you want upper case?  Press Ok for Yes or Cancel for No " )
+var lower = confirm("Do you want lower case? Press Ok for Yes or Cancel for No ")
 
 
 
